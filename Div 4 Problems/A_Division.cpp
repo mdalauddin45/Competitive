5bc8d5bdc81298724551afc3c620c69925f2e6f1
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-07-06 21:58:56 +0600

    Division 4

---
+++ Div 4 Problems/A_Division.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef pair<int, int> p;
+typedef long long ll;
+typedef long long int lli;
+const int N=1e5+7;
+int nums[N];
+ 
+#define cn(n)           int n;cin>>n;
+#define w(t)            int t;cin>>t;while(t--)
+#define Alauddin        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define pi              M_PI 
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int main()
+{
+    Alauddin
+    w(t){
+        cn(n);
+        if(1900<=n) cout<<"Division 1"<<endl;
+        else if(1600<=n) cout<<"Division 2"<<endl;
+        else if(1400<=n) cout<<"Division 3"<<endl;
+        else cout<<"Division 4"<<endl;
+    }
+    return 0;
+}
