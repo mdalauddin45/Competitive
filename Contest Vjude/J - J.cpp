a088778657b161d14b5fca29e74ea789d7d25e5c
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-07-09 21:30:12 +0600

    J done

---
+++ Contest Vjude/J - J.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef pair<int, int> p;
+typedef long long ll;
+typedef long long int lli;
+const int N=1e5+7;
+int nums[N];
+ 
+#define cn(n)           int n;cin>>n;
+#define w(t)            int t;cin>>t;while(t--)
+#define Alauddin        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define pi              M_PI 
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+ 
+int main()
+{
+    Alauddin
+    int a,b;cin>>a>>b;
+    cout<<a+b<<"1"<<endl;
+    return 0;
+}
